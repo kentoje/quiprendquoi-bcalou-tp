c34a40c73eb70eac91f0846f75cc34622d638482
kentoje <k.monthubert@gmail.com>
2020-03-16 18:08:38 +0100

    feat(Party): Add DELETE and POST action

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require("node-fetch");
+const methodOverride = require('method-override');
 const app = express();
 const port = process.env.PORT;
 
@@ -10,6 +11,7 @@ app.set('view engine', 'pug');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   res.render('index', {
@@ -46,6 +48,38 @@ app.post('/party', async (req, res) => {
 
     const { _id } = await data.json();
     res.redirect(`/party/${_id}`);
+  } catch (error) {
+    res.send(error)
+  }
+});
+
+app.post('/party/:id/items', async (req, res) => {
+  try {
+    const data = await fetch(`${process.env.API_URL}/party/${req.params.id}/items`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(req.body)
+    });
+
+    await data.json();
+    res.redirect('back');
+  } catch (error) {
+    res.send(error)
+  }
+});
+
+app.delete('/party/:id/items/:itemId', async (req, res) => {
+  const { id, itemId } = req.params
+
+  try {
+    const data = await fetch(`${process.env.API_URL}/party/${id}/items/${itemId}`, {
+      method: 'DELETE',
+    });
+    await data.json();
+    res.redirect('back');
   } catch (error) {
     res.send(error)
   }
