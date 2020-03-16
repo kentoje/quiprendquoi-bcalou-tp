7b6500e056e3529aa30e5bf3b621060eb8f0d513
kentoje <k.monthubert@gmail.com>
2020-03-16 11:43:31 +0100

    feat(Package): Add nodemon

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', function(req, res) {
-  res.send('Hello world!');
+app.get('/', (req, res) => {
+  res.send('yo');
 });
 
-app.listen(port, () => console.log(`Front app listening on port ${port}! At http://localhost:${port}`));
+app.listen(port, _ => console.log(`Front app listening on port ${port}! At http://localhost:${port}`));
