91dcf55b85d489ab051b7d604cd2dc378b909ab5
kentoje <k.monthubert@gmail.com>
2020-03-16 12:30:52 +0100

    feat(Package): Add BodyParser

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT;
 
 app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.render('index', {
