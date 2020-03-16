9a758d8a63258a408fad0244d4d124a913e5d33b
kentoje <k.monthubert@gmail.com>
2020-03-16 11:50:44 +0100

    feat(Package): Add pug and index pug file

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('yo');
+  res.render('index');
 });
 
 app.listen(port, _ => console.log(`Front app listening on port ${port}! At http://localhost:${port}`));
