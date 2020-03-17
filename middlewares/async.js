d70d872cd12ba952bdf9e55fc378f2000cd0d125
kentoje <k.monthubert@gmail.com>
2020-03-17 11:48:54 +0100

    refacto(App): Add Middlewares

--- middlewares/async.js
+++ middlewares/async.js
@@ -1,9 +1,10 @@
-module.exports = function asyncMiddleware(handler) {
-  return async (req, res, next) => {
+module.exports = asyncMiddleware = (handler) => (
+  async (req, res, next) => {
     try {
       await handler(req, res);
-    } catch (ex) {
-      next(ex)
+    } catch (error) {
+      next(error);
+      res.send('Something went wrong');
     }
   }
-};
+);
--- routes/parties.js
+++ routes/parties.js
@@ -1,123 +1,92 @@
 const fetch = require('node-fetch');
 const express = require('express');
+const asyncMiddleware = require('../middlewares/async');
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-  try {
-    const data = await fetch(`${process.env.API_URL}/party`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(req.body)
-    });
+router.post('/', asyncMiddleware(async (req, res) => {
+  const data = await fetch(`${process.env.API_URL}/party`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(req.body)
+  });
 
-    const { _id } = await data.json();
-    res.redirect(`/party/${_id}`);
-  } catch (error) {
-    res.send(error);
-  }
-});
+  const { _id } = await data.json();
+  res.redirect(`/party/${_id}`);
+}));
 
-router.get('/:id', async (req, res) => {
-  try {
-    const data = await fetch(`${process.env.API_URL}/party/${req.params.id}`);
-    const response = await data.json();
-    const { name, _id } = response;
+router.get('/:id', asyncMiddleware(async (req, res) => {
+  const data = await fetch(`${process.env.API_URL}/party/${req.params.id}`);
+  const response = await data.json();
+  const { name, _id } = response;
 
-    res.render('party', {
-      party: response,
-      title: name,
-      url: `${process.env.FRONT_URL}:${process.env.PORT}/party/${_id}`,
-    });
-  } catch (error) {
-    res.send(error);
-  }
-});
+  res.render('party', {
+    party: response,
+    title: name,
+    url: `${process.env.FRONT_URL}:${process.env.PORT}/party/${_id}`,
+  });
+}));
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', asyncMiddleware(async (req, res) => {
   const { id } = req.params;
+  const data = await fetch(`${process.env.API_URL}/party/${id}`, {
+    method: 'DELETE',
+  });
+  await data.json();
+  res.redirect('/');
+}));
 
-  try {
-    const data = await fetch(`${process.env.API_URL}/party/${id}`, {
-      method: 'DELETE',
-    });
-    await data.json();
-    res.redirect('/');
-  } catch (error) {
-    res.send(error);
-  }
-});
-
-router.patch('/:id', async (req, res) => {
+router.patch('/:id', asyncMiddleware(async (req, res) => {
   const { id } = req.params;
+  const data = await fetch(`${process.env.API_URL}/party/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Accept': 'application/json',
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(req.body)
+  });
+  await data.json();
+  res.redirect('back');
+}));
 
-  try {
-    const data = await fetch(`${process.env.API_URL}/party/${id}`, {
-      method: 'PATCH',
-      headers: {
-        'Accept': 'application/json',
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(req.body)
-    });
-    await data.json();
-    res.redirect('back');
-  } catch (error) {
-    res.send(error);
-  }
-});
+router.post('/:id/items', asyncMiddleware(async (req, res) => {
+  const data = await fetch(`${process.env.API_URL}/party/${req.params.id}/items`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(req.body)
+  });
 
-router.post('/:id/items', async (req, res) => {
-  try {
-    const data = await fetch(`${process.env.API_URL}/party/${req.params.id}/items`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(req.body)
-    });
+  await data.json();
+  res.redirect('back');
+}));
 
-    await data.json();
-    res.redirect('back');
-  } catch (error) {
-    res.send(error);
-  }
-});
-
-router.delete('/:id/items/:itemId', async (req, res) => {
+router.delete('/:id/items/:itemId', asyncMiddleware(async (req, res) => {
   const { id, itemId } = req.params;
+  const data = await fetch(`${process.env.API_URL}/party/${id}/items/${itemId}`, {
+    method: 'DELETE',
+  });
+  await data.json();
+  res.redirect('back');
+}));
 
-  try {
-    const data = await fetch(`${process.env.API_URL}/party/${id}/items/${itemId}`, {
-      method: 'DELETE',
-    });
-    await data.json();
-    res.redirect('back');
-  } catch (error) {
-    res.send(error);
-  }
-});
-
-router.patch('/:id/items/:itemId', async (req, res) => {
+router.patch('/:id/items/:itemId', asyncMiddleware(async (req, res) => {
   const { id, itemId } = req.params;
-
-  try {
-    const data = await fetch(`${process.env.API_URL}/party/${id}/items/${itemId}`, {
-      method: 'PATCH',
-      headers: {
-        'Accept': 'application/json',
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(req.body)
-    });
-    await data.json();
-    res.redirect('back');
-  } catch (error) {
-    res.send(error);
-  }
-});
+  const data = await fetch(`${process.env.API_URL}/party/${id}/items/${itemId}`, {
+    method: 'PATCH',
+    headers: {
+      'Accept': 'application/json',
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(req.body)
+  });
+  await data.json();
+  res.redirect('back');
+}));
 
 module.exports = router;
