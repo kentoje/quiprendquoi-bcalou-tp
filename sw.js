92de67acb36055b6df28061fa531114d26f7c93d
kentoje <k.monthubert@gmail.com>
2020-03-18 11:58:55 +0100

    feat(ServiceWorker): Add access to cached Party pages when offline

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+const isPartyPage = url => /party\/[a-zA-Z0-9]*$/.test(url);
+
 addEventListener('install', event => {
   event.waitUntil(
     caches.open('offline').then((cache) => {
@@ -9,7 +11,27 @@ addEventListener('install', event => {
 addEventListener('fetch', event => {
   if (event.request.headers.get('Accept').includes('text/html')) {
     event.respondWith(
-      fetch(event.request).catch(_ => caches.match('offline.html')),
+      fetch(event.request)
+        .then(response => {
+          if (isPartyPage(event.request.url)) {
+            const copy = response.clone();
+            caches
+              .open('parties')
+              .then(cache => cache.put(event.request, copy));
+            return response;
+          } else {
+            return response;
+          }
+        })
+        .catch(_ => {
+          if (isPartyPage(event.request.url)) {
+            return caches
+              .match(event.request)
+              .catch(_ => caches.match('offline.html'));
+          } else {
+            return caches.match('offline.html');
+          }
+        })
     );
   }
 });
