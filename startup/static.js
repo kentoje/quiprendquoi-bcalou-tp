2e1c4d1007b616b731dcc18d52c3d1feded6dd73
kentoje <k.monthubert@gmail.com>
2020-03-19 17:02:19 +0100

    fix(App): Fix audits feedback

--- startup/static.js
+++ startup/static.js
@@ -3,4 +3,5 @@ const express = require('express');
 module.exports = app => {
   app.use(express.static('public'));
   app.use(express.static('views/pwa'));
+  app.use(express.static('images/icons'));
 }
